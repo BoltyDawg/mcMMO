3124e896015cfeda15c9b3b76d71ae5afb6c3748
Jason Bolton <36057500+BoltyDawg@users.noreply.github.com>
2018-08-30 13:17:39 -0400

    Added support for my custom armors

--- src/main/java/com/gmail/nossr50/skills/repair/RepairManager.java
+++ src/main/java/com/gmail/nossr50/skills/repair/RepairManager.java
@@ -24,6 +24,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.block.data.BlockData;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
 
